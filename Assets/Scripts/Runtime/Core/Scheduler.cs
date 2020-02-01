d26243ddd533544b148b638eda478a5f3bed6520
Peter Mehlmann <peter@petermehlmann.com>
2020-02-01 21:40:20 +0100

    Moved Ticks from Manager to Scheduler

--- Assets/Scripts/Runtime/Core/Scheduler.cs
+++ Assets/Scripts/Runtime/Core/Scheduler.cs
@@ -15,7 +15,9 @@ public class Scheduler : Monobehaviour
             return;
         }
         scenario = GetComponent<Scenario>();
+        // This will not work if there is no devparent
         DevParent = GetComponent<DevParent>();
+        taskList.Tick(scenario.GetStatus());
     }
 
     public void StartDay(){
@@ -42,6 +44,28 @@ public class Scheduler : Monobehaviour
         }
 	}
 
+
+void Update()
+    {
+		currentTime += Time.deltaTime;
+		if(currentTime > tickTime)
+		{
+			currentTime -= tickTime;
+			++currentTick;
+			bool isNewDay = currentTick >= ticksPerDay;
+			if (isNewDay)
+				currentTick = 0;
+
+			scenario.Tick(isNewDay);
+			taskList.Tick(scenario.GetStatus());
+		}
+    }
+
+    public float tickTime;
+    public int ticksPerDay;
     private GameObject DevParent;
     private Scenario scenario;
+    private float currentTime;
+    private int currentTick;
+
 }
